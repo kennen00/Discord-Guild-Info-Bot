a67796edc1d61c2639ecb9f92cb3f577eeb1d624
Kennen DeRenard <kdere004@ucr.edu>
2017-05-18 15:59:50 -0700

    Added notes and added specific channel messaging

--- bot.py
+++ bot.py
@@ -15,18 +15,16 @@ def on_read():
 @myBot.event
 @asyncio.coroutine
 def on_message(message):
-    if ("hello" in message.content or "hi" in message.content or "hey" in message.content or "hola" in message.content):
-        yield from myBot.send_message(message.channel, "Hello, Hello?")
+    if (message.channel.id == "314867907739910165"):
+        if ("hello" in message.content):
+            yield from myBot.send_message(myBot.get_channel("314867907739910165"), "Hello, Hello?")
+        elif ("purpose" in message.content):
+            yield from myBot.send_message(myBot.get_channel("314867907739910165"), "Did you say Purpose? HA! I'd tell you what it is if it was in my database!!")
     yield from myBot.process_commands(message)
 
 @myBot.command()
 @asyncio.coroutine
 def hello(*args):
     yield from myBot.say("Hello, world!")
-
-@myBot.command()
-@asyncio.coroutine
-def notOntoKennen(*args):
-    yield from myBot.say("You're not onto Kennen :)")
 
 myBot.run("MzE0NTI0NTk0MjA1NjIyMjc0.C_5eKQ.bt2Tqhs_1sGSroG8zHTI2TfT-ko")
